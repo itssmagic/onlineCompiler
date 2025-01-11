aa8d611eb0c4153bd326a8ca3019ef4088b426d2
Shubh Engineer <itssmagic321@gmail.com>
2025-01-11 17:51:20 +0530

    tmp file added

--- backend/generateInputFile.js
+++ backend/generateInputFile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuid } = require('uuid');
 
-const dirInputs = path.join(__dirname, 'inputs');
+const dirInputs = path.join('/tmp', 'inputs');
 
 if (!fs.existsSync(dirInputs)) {
     fs.mkdirSync(dirInputs, { recursive: true });
