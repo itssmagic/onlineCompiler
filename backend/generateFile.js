2c847bbbcd0508057ca702a21f61eca1c38e4aab
Shubh Engineer <itssmagic321@gmail.com>
2025-01-11 21:40:11 +0530

    update cors

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuid } = require('uuid');
 
-const dirCodes = path.join(__dirname, 'codes');
+//const dirCodes = path.join(__dirname, 'codes');
+const dirCodes = "/tmp";
 
 if (!fs.existsSync(dirCodes)) {
     fs.mkdirSync(dirCodes, { recursive: true });
